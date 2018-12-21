e18e9bd313389ebdcf6f30327ca100635774ba3f
PhamQuynh <quynhpn62@wru.vn>
2018-12-21 11:21:59 +0700

    fix giao dien home

--- Sources/application/views/site/header_site_view.php
+++ Sources/application/views/site/header_site_view.php
@@ -6,31 +6,30 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#" style="padding-top: 25px;">
-              <img src="<?php echo base_url(); ?>assets/img/aaa.png" alt="logo" style="width:70px; height:auto;">
+            <a class="nav-link" href="#" style="padding: 5px 90px 5px 90px;">
+              <img src="<?php echo base_url(); ?>assets/img/aaa.png" alt="logo" style="width:80px; height:auto;">
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url();?>/index.php/home/index"style="padding-top:40px; font-size:25px; color:black;">TRANG CHỦ</a>
+            <a class="nav-link" href="<?php echo base_url();?>/index.php/home/index"style="padding-top:25px; font-size:25px; color:black;">TRANG CHỦ</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url();?>/index.php/introduce/index" style="padding-top:40px; font-size:25px; color:black;">GIỚI THIỆU</a>
+            <a class="nav-link" href="<?php echo base_url();?>/index.php/introduce/index" style="padding-top:25px; font-size:25px; color:black;">GIỚI THIỆU</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url();?>/index.php/price/index"style="padding-top:40px; font-size:25px; color:black;">BẢNG GIÁ</a>
+            <a class="nav-link" href="<?php echo base_url();?>/index.php/price/index"style="padding-top:25px; font-size:25px; color:black;">BẢNG GIÁ</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url();?>/index.php/setcalendar/index"style="padding-top:40px; font-size:25px; color:black;">ĐẶT LỊCH</a>
+            <a class="nav-link" href="<?php echo base_url();?>/index.php/setcalendar/index"style="padding-top:25px; font-size:25px; color:black;">ĐẶT LỊCH</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url();?>/index.php/hairstyle/index"style="padding-top:40px; font-size:25px; color:black;">TÓC ĐẸP</a>
+            <a class="nav-link" href="<?php echo base_url();?>/index.php/hairstyle/index"style="padding-top:25px; font-size:25px; color:black;">TÓC ĐẸP</a>
           </li>
         
         </ul>
       </div>
     </nav>
    </div>
-   
-</div>
+  </div>
 </div>
