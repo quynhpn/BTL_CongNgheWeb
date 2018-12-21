46feb92a6598e445f113ac06087a900b384dfb23
PhamQuynh <quynhpn62@wru.vn>
2018-12-21 14:32:16 +0700

    fix footer

--- Sources/application/views/site/footer_site_view.php
+++ Sources/application/views/site/footer_site_view.php
@@ -1,30 +1,30 @@
 <footer>
-   <div class="row"style="background-color:#cccccc;">
+   <div class="row"style="background-color:black; color:white;">
       <div class="container" >
-      <div class="row mt-5">
-        <div class="col-sm-6 col-md-4 footer-navigation">
-            <h4>Cửa hàng cắt tóc nam BARBER SHOP</h4>
-            <p class="links mt-3"><a href="#">Home</a><strong>·</strong><a href="#">Blog</a><strong>·</strong><a href="#">Pricing</a><strong>·</strong><a href="#">About</a><strong>·</strong><a href="#">Faq</a><strong>·</strong><a href="#">Contact</a></p>
-            <p class="company-name mt-4">Company Name © 2015</p>
+      <div class="row mt-3">
+        <div class="col-sm-6 col-md-4 footer-navigation mt-3">
+            <img src="<?php echo base_url(); ?>assets/img/aaa.png" alt="logo" style="width:150px; height:auto;">
         </div>
-        <div class="col-sm-6 col-md-5 footer-contacts" style="font-size:20px;">
+        <div class="col-sm-6 col-md-5 footer-contacts mt-5" style="font-size:20px;">
             <div>
-              <span class="fa fa-map-marker footer-contacts-icon mt-3"> Tây Sơn, Đống175 Tây Đa, Hà Nội</span>              
+              <span class="fa fa-map-marker footer-contacts-icon mt-3">  Tây Sơn, Đống175 Tây Đa, Hà Nội</span>              
             </div>
             <div>
-              <span class="fa fa-phone footer-contacts-icon mt-3"> 0965637450</span>         
+              <span class="fa fa-phone footer-contacts-icon mt-3">  0965637450</span>         
             </div>
             <div>
-               <span class="fa fa-envelope footer-contacts-icon mt-3">barbermen@gmail.com</span>
+               <span class="fa fa-envelope footer-contacts-icon mt-3"> barbermen@gmail.com</span>
             </div>
         </div>
         <div class="clearfix"></div>
-        <div class="col-md-3 footer-about text-center mt-4">
+        <div class="col-md-3 footer-about mt-4">
             <div class="social-links social-icons" style="font-size:30px;">
-              <a href="#"><i class="fa fa-facebook"></i></a>
-              <a href="#"><i class="fa fa-twitter"></i></a>
-              <a href="#"><i class="fa fa-linkedin"></i></a>
-              <a href="#"><i class="fa fa-github"></i></a>
+              <pre>
+                <a href="#" style="color:white;"><i class="fa fa-facebook"></i></a>
+                <a href="#" style="color:white;"><i class="fa fa-twitter"></i></a>
+                <a href="#" style="color:white;"><i class="fa fa-linkedin"></i></a>
+                <a href="#" style="color:white;"><i class="fa fa-github"></i></a>
+              </pre>
             </div>
         </div>
     </div>
