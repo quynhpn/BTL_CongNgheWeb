7fd9e7d794077ce87a2a73d37209f344688440c6
PhamQuynh <quynhpn62@wru.vn>
2018-12-26 10:22:24 +0700

    slideshow

--- Sources/application/views/site/body_home_site_view.php
+++ Sources/application/views/site/body_home_site_view.php
@@ -1,4 +1,29 @@
-<img src="<?php echo base_url(); ?>assets/img/image_home.jpg" alt="home"style="width:100%;height:auto;" class="mb-4">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner mb-4">
+    <div class="carousel-item active">
+      <img class="d-block w-100" style="height:600px;" src="<?php echo base_url(); ?>assets/img/slideshow1.jpg" alt="home">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" style="height:600px;" src="<?php echo base_url(); ?>assets/img/image_home.jpg" alt="home">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" style="height:600px;" src="<?php echo base_url(); ?>assets/img/Hair_car.gif" alt="home">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 <div>
   <div class="container">
             <div class="row">
